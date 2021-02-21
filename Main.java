29cfea7c6644fd4ebd80c67a0a839ca0530d38e0
Kyle McDowell <kymcd21@icstudents.org>
2021-02-21 08:23:12 +0000

    AAAAAAAAA

--- Main.java
+++ Main.java
@@ -5,20 +5,30 @@ class Main {
   public static void main(String[] args) throws FileNotFoundException {
     File inFile = new File("beemovie.txt");
     Scanner input = new Scanner(inFile);
+    PrintWriter copyOfBeeMovie=new PrintWriter("bOutput.txt");
     int lineCount = 0;
     int charCount=0;
     int wordCount=0;
-    Scanner wordCounter=new scanner (inFile);
+    Scanner wordScanner=new Scanner (inFile);
     do {
 
       String linea = input.nextLine();
-      charCount=charCount+linea.length(); 
-      System.out.println(linea);
       lineCount++;
     } while (input.hasNextLine());
+
+    do{
+      String wordCountString=wordScanner.next();
+      charCount=charCount+wordCountString.length();
+      wordCount++;
+    }while(input.hasNext());
+    copyOfBeeMovie.println(charCount);
+    copyOfBeeMovie.println(lineCount);
+    copyOfBeeMovie.println(wordCount);
     System.out.println(charCount);
     System.out.println(lineCount);
+    System.out.println(wordCount);
     input.close();
-
+    copyOfBeeMovie.close();
+    wordScanner.close();
   }
 }
